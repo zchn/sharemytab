790efee04a3a57d5b36298743af8f1a4909c0d1b
Kevin Chen <kevin.chen@kchen-mbp.local>
2013-05-06 02:40:46 -0700

    i

--- ext/livelecture/background.js
+++ ext/livelecture/background.js
@@ -1,8 +1,8 @@
 
 function checkForValidUrl(tabId, changeInfo, tab) {
-  if (tab.url.indexOf('/present#') > -1) {
+  //if (tab.url.indexOf('/present#') > -1) {
     chrome.pageAction.show(tabId);
-  }
+  //}
 };
 
 // Listen for any changes to the URL of any tab.
